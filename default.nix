35602eb9eca5bbab7bbd2925cc77473a39ec499a
Marc Scholten <marc@digitallyinduced.com>
2021-10-25 09:43:42 +0200

    Use IHP v0.15

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
     ihp = builtins.fetchGit {
         url = "https://github.com/digitallyinduced/ihp.git";
-        rev = "f12733f8b0f6e9725d93b67051f36c896bc55edf";
+        ref = "refs/tags/v0.15.0";
     };
     haskellEnv = import "${ihp}/NixSupport/default.nix" {
         ihp = ihp;
